095e8959f34476e4f040e872f4be18fe0fdf084f
Arash Shalchian <shalchianarash@gmail.com>
2024-03-08 11:18:34 -0500

    Setup app

--- public/js/app.js
+++ public/js/app.js
@@ -25,7 +25,7 @@ weatherForm.addEventListener('submit',(e) => {
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    fetch('http://localhost:3000/weather?address=' + location)
+    fetch('/weather?address=' + location)
         .then((response) => {
             return response.json();
         })
